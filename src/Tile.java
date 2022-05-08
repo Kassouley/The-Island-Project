ac1f7ba9734a2a209f1a0659eae048d13993bb1c
Akunesquik <104908248+Akunesquik@users.noreply.github.com>
2022-05-08 12:20:23 +0200

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -6,26 +6,66 @@ import java.util.*;
  */
 public class Tile {
 
-    /**
-     * Default constructor
-     */
-    public Tile() {
-    }
+   
+   	private TilesType type;
+	private TilesEffect effect;
 
-    /**
-     * 
-     */
-    public int type;
 
-    /**
-     * 
-     */
-    public int effect;
+	/**
+	* Default constructor
+	*/
+	public Tiles() {
+		this.setHorizontalAlignment(SwingConstants.CENTER);		
+	}
 
-    /**
-     * 
-     */
-   public void discover(/*board*/) {
+	/**
+	* 
+	*/
+	public TilesType getType() {
+		return type;
+	}
+
+	/**
+	* 
+	*/
+	public void setType(TilesType type) {
+		this.type = type;
+		if(type == TilesType.BEACH) {
+			this.setIcon(new ImageIcon(Tiles.class.getResource("/sprites/Plage.png")));
+		} else if(type == TilesType.FOREST) {
+			this.setIcon(new ImageIcon(Tiles.class.getResource("/sprites/Foret.png")));
+		} else if(type == TilesType.MONTAINS){
+			this.setIcon(new ImageIcon(Tiles.class.getResource("/sprites/Montagne.png")));
+		} else {
+			this.setIcon(null);
+		}
+	}
+
+	/**
+	* 
+	*/
+	public TilesEffect getEffect() {
+		return effect;
+	}
+
+	/**
+	* 
+	*/
+	public void setEffect(TilesEffect effect) {
+		this.effect = effect;
+	}
+
+	/**
+	* 
+	*/
+	public void setPosition(int x, int y) {
+		this.setBounds(x, y, 90, 90);
+	}
+	
+	/**
+	* 
+	*/
+	public void discover(/*board*/) {
 		/*if(board[i][j].tile.getType() == Verte){
 		 * 	board[i][j].tile.applyeffect()
 		 * 	removeFromBoard tile
@@ -35,10 +75,10 @@ public class Tile {
 		 */
 	}
 
-    /**
-     * 
-     */
-   public void applyEffect() {
+	/**
+	* 
+	*/
+	public void applyEffect() {
 		switch(this.effect) {
 		case WHALE : this.whaleEffect(); break;
 		case WHALE_DEATH : this.whaleDeathEffect();break;
@@ -55,97 +95,97 @@ public class Tile {
 		default : break;
 		}
 	}
-    
-    /**
-     * 
-     */    
-    public void whaleEffect(/*board*/) {
+
+	/**
+	* 
+	*/    
+	public void whaleEffect(/*board*/) {
 		//selectionner une case de mer Uniquement
 		//Point(i,j)
 		//board[i][j]listePions.addToBoard(whale)	
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void whaleDeathEffect(/*board*/) {
 		//board[i][j].listePions.removeFromBoard(whale)
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void whaleLostEffect(/*board*/) {
 		//selectionner une baleine
 		//selectionne la case ou on veut la deplacer (mer inoccupée)
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void boatMoveEffect() {
 		//selectionnne un bateau
 		//le de placer de 1 a 3 cases
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void boatEffect() {
 		//board[i][j].tuiles = MER
 		//board[i][j].listePions.addToBoard(boat)
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void dolphinMoveEffect() {
 		//selectionnne un nageur
 		//le de placer de 1 a 3 cases
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void sharkEffect() {
 		//board[i][j].tuiles = MER
 		//board[i][j].listePions.addToBoard(shark)
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void sharkDeathEffect() {
 		//board[i][j].listePions.removeFromBoard(shark)
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void sharkLostEffect() {
 		//selectionner un requin
 		//selectionne la case ou on veut la deplacer (mer inoccupée)
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void seasnakeLostEffect() {
 		//selectionner un seasnake
 		//selectionne la case ou on veut la deplacer (mer inoccupée)
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void whirlpoolEffect() {
 		//if(board[i][j].tuile=MER)
 		//for(i=0;i<len(board[i][j].listePions);i++) removeFromBoard(pion)
 	}
-	
-    /**
-     * 
-     */
+
+	/**
+	* 
+	*/
 	public void volcanoEffect() {
 		//endgame
 	}
