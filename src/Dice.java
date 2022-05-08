4b0415270a6960203568ff5fb23f3e25ec12dab8
KoliNomis <104193308+KoliNomis@users.noreply.github.com>
2022-05-08 14:46:03 +0200

    Update Dice
    Déclaration de la 1.0 de Dice

--- src/Dice.java
+++ src/Dice.java
@@ -1,23 +1,129 @@
+/*
+ * Nom de classe : Dice
+ *
+ * Description   : Gestion du dé du jeu The Island 
+ *
+ * Version       : 1.0
+ *
+ * Date          : 08/05/2022
+ * 
+ * Copyright     : Loïk Simon
+ */
 
-import java.util.*;
+import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
 /**
+ * <p>
+ * Gestion du dé du jeu The Island
+ * </p>
  * 
+ * @version 1.0
+ * 
+ * @author Loïk Simon
  */
 public class Dice {
 
     /**
-     * Default constructor
-     */
-    public Dice() {
-    }
+	 * <p>
+	 * Constructeur par défaut
+	 * </p>
+	 */
+	public Dice() {
+		this.seaSnakeList.add(new DiceAnimation("Images/Animation/seaSnake/seaSnake1.gif"));
+		this.seaSnakeList.add(new DiceAnimation("Images/Animation/seaSnake/seaSnake2.gif"));
+		this.sharkList.add(new DiceAnimation("Images/Animation/shark/shark1.gif"));
+		this.sharkList.add(new DiceAnimation("Images/Animation/shark/shark2.gif"));
+		this.whaleList.add(new DiceAnimation("Images/Animation/whale/whale1.gif"));
+		this.whaleList.add(new DiceAnimation("Images/Animation/whale/whale2.gif"));
+	}
 
 
     /**
-     * 
-     */
-    public void roll() {
-        // TODO implement here
-    }
+	 * <p>
+	 * Retourne le résultat d'un lancé de dé sous forme de int.
+	 * </p>
+	 * 
+	 * @since 1.0
+	 */
+	public int roll() {
+		return (int) ((Math.random() * (3 - 1)) + 1);
+	}
+
+    /**
+	 * <p>
+	 * Lance une animation du dé.
+	 * </p>
+	 * 
+	 * @param panelAnimation JPanel sur lequel placer l'animation.
+	 * @param layeredPane JLayeredPane sur lequel placer le JPanel d'animation.
+	 * @param result Résultat du dé à animer.
+	 * @since 1.0
+	 */
+	public void startAnimation(JPanel panelAnimation, JLayeredPane layeredPane, int result) {
+		int animationNumber;
+		switch (result) {
+			case 1:
+				animationNumber = (int) (Math.random() * this.seaSnakeList.size());
+				panelAnimation.add(this.seaSnakeList.get(animationNumber).getLabel());
+				break;
+
+			case 2:
+				animationNumber = (int) (Math.random() * this.sharkList.size());
+				panelAnimation.add(this.sharkList.get(animationNumber).getLabel());
+				break;
+
+			case 3:
+				animationNumber = (int) (Math.random() * this.whaleList.size());
+				panelAnimation.add(this.whaleList.get(animationNumber).getLabel());
+				break;
+		
+			default:
+				System.err.print("Dans Dice.java, dice.startAnimation : résultat non compatible");
+				break;
+		}
+
+		layeredPane.add(panelAnimation, 3);
+	}
+
+    /**
+	 * <p>
+	 * Arrête l'animation du dé.
+	 * </p>
+	 * 
+	 * @param panelAnimation JPanel sur lequel est placé l'animation.
+	 * @param layeredPane JLayeredPane sur lequel est placé le JPanel d'animation.
+	 * @since 1.0
+	 */
+	public void endAnimation(JPanel panelAnimation, JLayeredPane layeredPane) {
+		panelAnimation.setVisible(false);
+		panelAnimation.repaint();
+		EventQueue.invokeLater(() -> layeredPane.remove(panelAnimation));
+	}
+
+    /**
+	 * <p>
+	 * Liste de d'animation du résultat "seaSnake".
+	 * </p>
+	 */
+	private List<DiceAnimation> seaSnakeList = new ArrayList<DiceAnimation>();
+
+    /**
+	 * <p>
+	 * Liste de d'animation du résultat "shark".
+	 * </p>
+	 */
+	private List<DiceAnimation> sharkList = new ArrayList<DiceAnimation>();
+
+    /**
+	 * <p>
+	 * Liste de d'animation du résultat "whale".
+	 * </p>
+	 */
+	private List<DiceAnimation> whaleList = new ArrayList<DiceAnimation>();
 
 }
