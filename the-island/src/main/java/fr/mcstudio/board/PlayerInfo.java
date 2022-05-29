6fd8661da07a2433e766e14e6c3fe9c7728fa3ec
Kassouley <104192472+Kassouley@users.noreply.github.com>
2022-05-29 17:01:30 +0200

    Update Side Bar

--- the-island/src/main/java/fr/mcstudio/board/PlayerInfo.java
+++ the-island/src/main/java/fr/mcstudio/board/PlayerInfo.java
@@ -1,6 +1,9 @@
 package fr.mcstudio.board;
 
 import java.awt.*;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
 
@@ -13,14 +16,8 @@ public class PlayerInfo extends JLayeredPane {
 	private JLabel playerInfoLabel = new JLabel();
 	private JPanel playerInfoPanel = new JPanel();
 
-	private JLabel pseudoPlayer = new JLabel();
 	private JLabel avatarPlayer = new JLabel();
-	private JLabel explorersPlayer = new JLabel();
-	private JLabel explorersSaved = new JLabel();
-	private JLabel explorersDead = new JLabel();
-	private JLabel nextPlayer1 = new JLabel();
-	private JLabel nextPlayer2 = new JLabel();
-	private JLabel nextPlayer3 = new JLabel();
+	private List<JLabel> infosPlayer = new ArrayList<JLabel>();
 
 	public PlayerInfo(int resolution) {
 		super();
@@ -29,40 +26,74 @@ public class PlayerInfo extends JLayeredPane {
 		this.setLabel();
 		this.add(playerInfoLabel);
 
-		this.playerInfoPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		this.playerInfoPanel.setLayout(null);
 		this.playerInfoPanel.setBounds(0, 0, this.getWidth(), this.getHeight());
 		this.playerInfoPanel.setOpaque(false);
 		this.setLayer(playerInfoPanel, 1);
 		this.add(playerInfoPanel);
 
-		this.pseudoPlayer.setFont(new Font("/Font/Treasuremap.ttf", Font.PLAIN, 18));
-		this.pseudoPlayer.setVerticalAlignment(SwingConstants.TOP);
-		this.playerInfoPanel.add(pseudoPlayer);
+		Font sizedFont = null;
+		try {
+			InputStream is = ActionInfo.class.getResourceAsStream("/Font/Treasuremap.ttf");
+			Font font = Font.createFont(Font.TRUETYPE_FONT, is);
+			sizedFont = font.deriveFont(18f);
+		} catch (Exception ex) {
+			System.err.println("Not loaded");
+		}
 
-		this.avatarPlayer.setVerticalAlignment(SwingConstants.TOP);
+		for (int i = 0; i < 7; i++) {
+			JLabel text = new JLabel();
+			text.setFont(sizedFont);
+			this.playerInfoPanel.add(text);
+			this.infosPlayer.add(text);
+		}
+
+		this.avatarPlayer.setBounds(15 * resolution / 90, 120 * resolution / 90,
+				256 * resolution / 90, 256 * resolution / 90);
 		this.playerInfoPanel.add(avatarPlayer);
-
-		this.explorersPlayer.setFont(new Font("/Font/Treasuremap.ttf", Font.PLAIN, 18));
-		this.explorersPlayer.setVerticalAlignment(SwingConstants.TOP);
-		this.playerInfoPanel.add(explorersPlayer);
-
-		this.explorersSaved.setFont(new Font("/Font/Treasuremap.ttf", Font.PLAIN, 18));
-		this.explorersSaved.setVerticalAlignment(SwingConstants.TOP);
-		this.playerInfoPanel.add(explorersSaved);
-
-		this.explorersDead.setFont(new Font("/Font/Treasuremap.ttf", Font.PLAIN, 18));
-		this.explorersDead.setVerticalAlignment(SwingConstants.TOP);
-		this.playerInfoPanel.add(explorersDead);
 
 	}
 
 	public void displayPlayerInfo(Game game, int resolution) {
+
 		Player currentPlayer = game.getCurrentPlayer();
-		this.pseudoPlayer.setText(currentPlayer.getPseudo());
+		Player[] players = game.getPlayers();
+		int textWidth = 0;
+		int textHeight = 0;
+		int i = 0;
+
+		int[][] pos = {
+				{ 137, 55 },
+				{ 139, 488 },
+				{ 139, 573 },
+				{ 139, 658 },
+				{ 139, 800 },
+				{ 139, 850 },
+				{ 139, 900 },
+		};
+
+		String[] textInfo = {
+				currentPlayer.getPseudo(),
+				Integer.toString(currentPlayer.getNumberExplorerAlive()),
+				Integer.toString(currentPlayer.getNumberExplorerSaved()),
+				Integer.toString(currentPlayer.getNumberExplorerDead()),
+				players[(game.getCurrentPlayerIndex() + 1) % players.length].getPseudo(),
+				players[(game.getCurrentPlayerIndex() + 2) % players.length].getPseudo(),
+				players[(game.getCurrentPlayerIndex() + 3) % players.length].getPseudo()
+		};
+
+		for (String text : textInfo) {
+
+			JLabel playerInfo = this.infosPlayer.get(i);
+			playerInfo.setText(text);
+			textWidth = playerInfo.getFontMetrics(playerInfo.getFont()).stringWidth(playerInfo.getText());
+			textHeight = playerInfo.getFontMetrics(playerInfo.getFont()).getHeight();
+			playerInfo.setBounds((pos[i][0] - textWidth / 2) * resolution / 90, pos[i][1] * resolution / 90, textWidth,
+					textHeight);
+			i++;
+		}
+
 		this.avatarPlayer.setIcon(currentPlayer.getAvatar());
-		this.explorersPlayer.setText(Integer.toString(currentPlayer.getNumberExplorerAlive()));
-		this.explorersSaved.setText(Integer.toString(currentPlayer.getNumberExplorerSaved()));
-		this.explorersDead.setText(Integer.toString(currentPlayer.getNumberExplorerDead()));
 	}
 
 	private void setPanelBoundsFromResolution(int resolution) {
